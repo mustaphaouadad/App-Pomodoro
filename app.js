e546786ec2ff153abab5302df44946c2640008c4
mustaphaouadad <ouadad.mustapha01@gmail.com>
2025-01-28 00:46:24 +0100

    Ajouter parti1 html and scc and js

---
+++ app.js
@@ -0,0 +1,42 @@
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+const timer = document.getElementById("timer");
+
+
+   let timeLeft = 1500;
+   let interval;
+
+   const updateTime = () =>{
+      const minutes =Math.floor(timeLeft/60);
+      const seconds = timeLeft % 60;
+      timer.innerHTML = 
+      `${minutes.toString().padStart(2,"0")}
+      :
+      ${seconds.toString().padStart(2,"0")}`;
+   };
+   const startTimer =() =>{
+    interval = setInterval(() => {
+        timeLeft--;
+        updateTime();
+        if (timeLeft === 0) {
+            clearInterval(interval);
+            alert("Time's up!");
+            timeLeft = 1500;
+            updateTime();
+            
+        }
+    },1000);
+    
+
+   };
+    
+   const stopTimer = () => clearInterval(interval);
+   const resetTimer = () =>{
+    clearInterval(interval);
+    timeLeft = 1500;
+    updateTime();
+   }
+   start.addEventListener("click",startTimer);
+   stop.addEventListener("click",stopTimer);
+   reset.addEventListener("click",resetTimer);
