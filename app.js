5473151835ce978e4e7a76c5f64dad3b2b436158
stouf <ouadad.mustapha01@gmail.com>
2025-01-31 15:17:07 +0100

    ajouter le pause de pomododro

--- app.js
+++ app.js
@@ -120,6 +120,11 @@ function toggleComplete(index) {
 
 
     // --------------------------
+
+
+//    ----------------------butoooon
+document.getElementById("focus").addEventListener("click", function () {
+    document.getElementById("timer").textContent = "25:00";
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
 const reset = document.getElementById("reset");
@@ -163,17 +168,97 @@ const timer = document.getElementById("timer");
    stop.addEventListener("click",stopTimer);
    reset.addEventListener("click",resetTimer);
 
-
-//    ----------------------butoooon
-document.getElementById("focus").addEventListener("click", function () {
-    document.getElementById("timer").textContent = "25:00";
 });
 
 document.getElementById("shortbreak").addEventListener("click", function () {
-    document.getElementById("timer").textContent = "5:00";
+document.getElementById("timer").textContent = "5:00";
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+const timer = document.getElementById("timer");
+
+
+   let timeLeft = 300;
+   let interval;
+
+   const updateTime = () =>{
+      const minutes =Math.floor(timeLeft/60);
+      const seconds = timeLeft % 60;
+      timer.innerHTML = 
+      `${minutes.toString().padStart(2,"0")}
+      :
+      ${seconds.toString().padStart(2,"0")}`;
+   };
+   const startTimer =() =>{
+    interval = setInterval(() => {
+        timeLeft--;
+        updateTime();
+        if (timeLeft === 0) {
+            clearInterval(interval);
+            alert("Time's up!");
+            timeLeft = 300;
+            updateTime();
+            
+        }
+    },1000);
+    
+
+   };
+    
+   const stopTimer = () => clearInterval(interval);
+   const resetTimer = () =>{
+    clearInterval(interval);
+    timeLeft = 300;
+    updateTime();
+   }
+   start.addEventListener("click",startTimer);
+   stop.addEventListener("click",stopTimer);
+   reset.addEventListener("click",resetTimer);
 });
 
 document.getElementById("longBreak").addEventListener("click", function () {
     document.getElementById("timer").textContent = "10:00";
+    const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+const timer = document.getElementById("timer");
+
+
+   let timeLeft = 600;
+   let interval;
+
+   const updateTime = () =>{
+      const minutes =Math.floor(timeLeft/60);
+      const seconds = timeLeft % 60;
+      timer.innerHTML = 
+      `${minutes.toString().padStart(2,"0")}
+      :
+      ${seconds.toString().padStart(2,"0")}`;
+   };
+   const startTimer =() =>{
+    interval = setInterval(() => {
+        timeLeft--;
+        updateTime();
+        if (timeLeft === 0) {
+            clearInterval(interval);
+            alert("Time's up!");
+            timeLeft = 600;
+            updateTime();
+            
+        }
+    },1000);
+    
+
+   };
+    
+   const stopTimer = () => clearInterval(interval);
+   const resetTimer = () =>{
+    clearInterval(interval);
+    timeLeft = 600;
+    updateTime();
+   }
+   start.addEventListener("click",startTimer);
+   stop.addEventListener("click",stopTimer);
+   reset.addEventListener("click",resetTimer);
 });
 
